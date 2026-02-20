59356983804c85bdde19727c44c39cad1e2f312b
dluksa20 <dluksa20@gmail.com>
2026-02-20 15:17:43 +0000

    SendMail functionality added

---
+++ lib/SendMail.ts
@@ -0,0 +1,11 @@
+import nodemailer from 'nodemailer';
+
+export const sendMail = async (to: string, subject: string, text: string) => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD,
+        },
+    });
+};
