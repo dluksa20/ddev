177187367de5330062ec29062d42cca36e7fa10e
dluksa20 <dluksa20@gmail.com>
2026-02-24 23:49:05 +0000

    local variables added

--- lib/email.ts
+++ lib/email.ts
@@ -1,11 +1,10 @@
 import nodemailer from 'nodemailer';
 
-export const sendMail = async (to: string, subject: string, text: string) => {
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASSWORD,
-        },
-    });
-};
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        type: 'OAuth2',
+        user: "dluksa27@gmail.com",
+        
+    }
+})
